f7a15e7da6fe30c567196e8d19c08f20f385c3bd
DungNguyen2309 <116014122+DungNguyen2309@users.noreply.github.com>
2023-11-30 16:42:14 +0700

    update incomingmanager

--- QuanLyCafe/QuanLyCF/Winform-Login-UI-Design-main/Login Page Design UI/FQLPhieuNhap.Designer.cs
+++ QuanLyCafe/QuanLyCF/Winform-Login-UI-Design-main/Login Page Design UI/FQLPhieuNhap.Designer.cs
@@ -28,36 +28,197 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.dtgPN = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMaPN = new System.Windows.Forms.TextBox();
+            this.txtDaTra = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNV = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtTongTien = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtpNgayLap = new System.Windows.Forms.DateTimePicker();
+            this.txtCongNo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCapNhat = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnUpdateAll = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgPN)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dtgPN
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(801, 450);
-            this.dataGridView1.TabIndex = 0;
+            this.dtgPN.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dtgPN.Location = new System.Drawing.Point(12, 207);
+            this.dtgPN.Name = "dtgPN";
+            this.dtgPN.RowHeadersWidth = 51;
+            this.dtgPN.RowTemplate.Height = 24;
+            this.dtgPN.Size = new System.Drawing.Size(596, 454);
+            this.dtgPN.TabIndex = 0;
+            this.dtgPN.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dtgPN_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 16);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mã PN";
+            // 
+            // txtMaPN
+            // 
+            this.txtMaPN.Location = new System.Drawing.Point(75, 35);
+            this.txtMaPN.Name = "txtMaPN";
+            this.txtMaPN.Size = new System.Drawing.Size(100, 22);
+            this.txtMaPN.TabIndex = 2;
+            // 
+            // txtDaTra
+            // 
+            this.txtDaTra.Location = new System.Drawing.Point(75, 94);
+            this.txtDaTra.Name = "txtDaTra";
+            this.txtDaTra.Size = new System.Drawing.Size(100, 22);
+            this.txtDaTra.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "đã trả";
+            // 
+            // txtNV
+            // 
+            this.txtNV.Location = new System.Drawing.Point(283, 91);
+            this.txtNV.Name = "txtNV";
+            this.txtNV.Size = new System.Drawing.Size(100, 22);
+            this.txtNV.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(191, 97);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(86, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "nhân viên lập";
+            // 
+            // txtTongTien
+            // 
+            this.txtTongTien.Location = new System.Drawing.Point(283, 35);
+            this.txtTongTien.Name = "txtTongTien";
+            this.txtTongTien.Size = new System.Drawing.Size(100, 22);
+            this.txtTongTien.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(210, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 16);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "tổng tiền";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // dtpNgayLap
+            // 
+            this.dtpNgayLap.Location = new System.Drawing.Point(105, 151);
+            this.dtpNgayLap.Name = "dtpNgayLap";
+            this.dtpNgayLap.Size = new System.Drawing.Size(261, 22);
+            this.dtpNgayLap.TabIndex = 9;
+            // 
+            // txtCongNo
+            // 
+            this.txtCongNo.Location = new System.Drawing.Point(479, 35);
+            this.txtCongNo.Name = "txtCongNo";
+            this.txtCongNo.Size = new System.Drawing.Size(100, 22);
+            this.txtCongNo.TabIndex = 11;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(389, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(84, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "tổng công nợ";
+            // 
+            // btnCapNhat
+            // 
+            this.btnCapNhat.Location = new System.Drawing.Point(422, 94);
+            this.btnCapNhat.Name = "btnCapNhat";
+            this.btnCapNhat.Size = new System.Drawing.Size(147, 29);
+            this.btnCapNhat.TabIndex = 12;
+            this.btnCapNhat.Text = "cập nhật";
+            this.btnCapNhat.UseVisualStyleBackColor = true;
+            this.btnCapNhat.Click += new System.EventHandler(this.btnCapNhat_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(25, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "ngày lập";
+            // 
+            // btnUpdateAll
+            // 
+            this.btnUpdateAll.Location = new System.Drawing.Point(422, 150);
+            this.btnUpdateAll.Name = "btnUpdateAll";
+            this.btnUpdateAll.Size = new System.Drawing.Size(147, 29);
+            this.btnUpdateAll.TabIndex = 14;
+            this.btnUpdateAll.Text = "cập nhật tất cả";
+            this.btnUpdateAll.UseVisualStyleBackColor = true;
+            this.btnUpdateAll.Click += new System.EventHandler(this.btnUpdateAll_Click);
             // 
             // FQLPhieuNhap
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(630, 673);
+            this.Controls.Add(this.btnUpdateAll);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnCapNhat);
+            this.Controls.Add(this.txtCongNo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dtpNgayLap);
+            this.Controls.Add(this.txtTongTien);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtNV);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtDaTra);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMaPN);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dtgPN);
             this.Name = "FQLPhieuNhap";
             this.Text = "FQLPhieuNhap";
             this.Load += new System.EventHandler(this.FQLPhieuNhap_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dtgPN)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dtgPN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMaPN;
+        private System.Windows.Forms.TextBox txtDaTra;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNV;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtTongTien;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtpNgayLap;
+        private System.Windows.Forms.TextBox txtCongNo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCapNhat;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnUpdateAll;
     }
 }
--- QuanLyCafe/QuanLyCF/Winform-Login-UI-Design-main/Login Page Design UI/FQLPhieuNhap.cs
+++ QuanLyCafe/QuanLyCF/Winform-Login-UI-Design-main/Login Page Design UI/FQLPhieuNhap.cs
@@ -14,6 +14,8 @@ namespace Login_Page_Design_UI
     public partial class FQLPhieuNhap : Form
     {
         IncomingsBLL incomingsBLL= new IncomingsBLL();
+        EmployeesBLL employeesBLL= new EmployeesBLL();  
+        Employee emp;
         public FQLPhieuNhap()
         {
             InitializeComponent();
@@ -21,7 +23,76 @@ namespace Login_Page_Design_UI
 
         private void FQLPhieuNhap_Load(object sender, EventArgs e)
         {
-            dataGridView1.DataSource = incomingsBLL.GetAllIncomings();
+            LoadPN();
+        }
+        public void LoadPN()
+        {
+            dtgPN.DataSource = incomingsBLL.GetAllIncomings();
+        }
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dtgPN_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtMaPN.Text = dtgPN.CurrentRow.Cells[0].Value.ToString();
+            dtpNgayLap.Value = DateTime.Parse(dtgPN.CurrentRow.Cells[1].Value.ToString());
+            txtTongTien.Text = dtgPN.CurrentRow.Cells[2].Value.ToString();
+            txtDaTra.Text = dtgPN.CurrentRow.Cells[3].Value.ToString();
+            emp = employeesBLL.GetEmployeeById(int.Parse(dtgPN.CurrentRow.Cells[4].Value.ToString()));
+            txtNV.Text = emp.FirstName + " " + emp.LastName;
+            txtCongNo.Text = (float.Parse(txtTongTien.Text) - float.Parse(txtDaTra.Text)).ToString();
+        }
+
+        private void btnCapNhat_Click(object sender, EventArgs e)
+        {
+            Incoming incoming = new Incoming {
+                IncomingID = int.Parse(txtMaPN.Text),
+                IncomingDate = dtpNgayLap.Value,
+                total_Price=float.Parse(txtTongTien.Text),
+                amount_paid=float.Parse(txtDaTra.Text)
+              
+            };
+            try
+            {
+                incomingsBLL.UpdateIncoming(incoming);
+                LoadPN();
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("That bai");
+            }
+
+        }
+
+        private void btnUpdateAll_Click(object sender, EventArgs e)
+        {
+            for(int i = 0; i < dtgPN.RowCount; i++)
+            {
+                Incoming incoming = new Incoming {
+                IncomingID = int.Parse(dtgPN.CurrentRow.Cells[0].Value.ToString()),
+                IncomingDate = DateTime.Parse(dtgPN.CurrentRow.Cells[1].Value.ToString()),
+                total_Price =float.Parse( dtgPN.CurrentRow.Cells[2].Value.ToString()),
+                amount_paid = float.Parse(dtgPN.CurrentRow.Cells[3].Value.ToString())
+
+               
+            };
+                try
+                {
+                    incomingsBLL.UpdateIncoming(incoming);
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+                
+               
+               
+            }
+            LoadPN();
         }
     }
 }
